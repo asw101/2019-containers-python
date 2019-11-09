91cc5d20cb7ee87069a4fcb5cef60aa47ebe1062
Aaron Wislang <Aaron.Wislang@microsoft.com>
2019-11-09 04:02:18 +0000

    fix

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,7 @@
 echo "hello world"
 #cp .devcontainer/keybindings.json /User/keybindings.json
 #cp .devcontainer/settings.json /User/settings.json
+cd .devcontainer/
 source 1-az.sh
 source 2-golang.sh
 source 3-misc.sh
