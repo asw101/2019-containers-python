fd335f1b15bdb3e0a6d460733ba18febe01a5bc2
Aaron Wislang <Aaron.Wislang@microsoft.com>
2019-11-09 21:57:47 +0000

    remove az, rust -> rustlang

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,8 +2,6 @@ echo "hello world"
 #cp .devcontainer/keybindings.json /User/keybindings.json
 #cp .devcontainer/settings.json /User/settings.json
 cd .devcontainer/
-source 1-az.sh
 source 2-golang.sh
 source 3-misc.sh
-source 4-rust.sh
-echo "goodbye world"
+source 4-rustlang.sh
