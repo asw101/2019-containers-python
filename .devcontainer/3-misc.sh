ea04e0ba066c994583edbb3b6d03c5f0f317d66e
Aaron Wislang <Aaron.Wislang@microsoft.com>
2019-11-10 04:38:01 +0000

    fix python3-venv

--- .devcontainer/3-misc.sh
+++ .devcontainer/3-misc.sh
@@ -1,6 +1,9 @@
 # jq
 sudo apt-get install -y jq
 
+# python3-venv
+sudo apt-get install -y python3-venv
+
 # hey
 go get -u github.com/rakyll/hey
 sudo echo 'export PATH=$PATH:$HOME/go/bin' >> $HOME/.profile
